3a3d1aabf44ca772e089eba9e16dbd6fdb467280
Sean Canton <eighteyes@gmail.com>
2016-07-26 12:09:02 -0700

    refactor for app id vs app name

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ module.exports = {
       return console.error("Firebase init needs token", token)
     }
 
-    var path = '/' + userId +'/'+ deviceId;
+    var path = '/' + userId +'/devices/'+ deviceId;
 
     debug('Init Firebase', userId, deviceId, path )
 
 
     var F = require( 'firebase' );
-    firebase = new F( 'https://admobilize.firebaseio.com/appconf' + path);
+    // firebase = new F( 'https://admobilize.firebaseio.com/appconf' + path);
+    firebase = new F( 'https://admobilize.firebaseio.com/deviceconf' + path);
 
     firebase.authWithCustomToken(token, function(err, authData) {
       if(err) {
@@ -40,6 +41,10 @@ module.exports = {
       } else {
         debug("User is logged out");
       }
+
+      // populate local app index
+      getAllApps();
+
       cb(null, { userId: authData.auth.uid })
     });
 
@@ -149,41 +154,141 @@ module.exports = {
       }, function ( e ) {
         if ( !_.isNull( s.val() ) ) cb( e )
       } )
+    },
+    meta: function( deviceId, cb ){
+      firebase.child('meta').once('value',  function ( s ) {
+        cb( null, s.val() )
+      }, function ( e ) {
+        cb( e )
+      })
     }
   },
 
   app: {
+    appKeys : appKeys,
     add: function ( appId, config ) {
       if (arguments.length === 2){
-        firebase.child( appId ).set( config )
+        firebase.child( 'apps/' + appId ).set( config )
+      }
+      if (arguments.length === 1){
+        // assume config, gen ID
+        firebase.child( 'apps/' + genID()+ '/config' ).set( config )
       }
     },
     set: function ( appId, config ) {
-      firebase.child( appId ).set( config )
+      firebase.child( 'apps/' +appId ).set( config )
     },
     update: function ( appId, config ) {
-      firebase.child( appId ).update( config )
+      firebase.child( 'apps/' +appId ).update( config )
     },
     get: function (  appId, cb ) {
       debug(appId.blue + ' (config)>'.grey)
-      firebase.child( appId ).once( 'value', function ( s ) {
+      firebase.child( 'apps/' + appId ).once( 'value', function ( s ) {
         cb( null, s.val() )
       }, function ( e ) {
         if ( !_.isNull( s.val() ) ) cb( e )
-      } )
+      })
+    },
+    getPolicy: function( appId, cb ){
+      firebase.child('apps/'+ appId + '/policy').on('value', function(data){
+        debug('app.policy>', data)
+        cb(null, data)
+      })
+    },
+    getMeta: function( appId, cb ){
+      firebase.child('apps/'+ appId + '/meta').on('value', function(data){
+        debug('app.meta>', data)
+        cb(null, data)
+      })
+    },
+    watchRuntime: function( appId, cb ){
+      firebase.child('apps/'+ appId + '/runtime').on('child_changed', function(data){
+        debug('app.runtime>', data)
+        cb(data.val())
+      })
+    },
+    watchConfig: function( appId, cb ){
+      firebase.child('apps/'+ appId + '/config').on('child_changed', function(data){
+        debug('app.config>', data)
+        cb(data.val())
+      })
+    },
+    getAll: getAllApps,
+    getConfigByName( appName, cb ){
+      var appId = getAppKeyFromName(appName);
+      firebase.child('apps/' + appId ).on('value', function(data){
+        cb(null, data.val().config );
+      })
+    },
+    getIDForName( appName, cb){
+      firebase.child('apps/').orderByKey().on('value', function(data){
+        var app = _.map(data, function (app) {
+          return (app.name == appName)
+        });
+        if ( app.length === 0 ){
+          return cb(new Error('No firebase application by that name:' + appName))
+        }
+        app = app[0];
+        cb(null, app.key );
+      })
     },
     remove: function ( appId, cb ) {
-      firebase.child( appId ).remove( cb );
+      appId = checkId(appId);
+      if ( appId ){
+        firebase.child( 'apps/' + appId ).remove( cb );
+      }
     },
     onInstall: function( cb ){
-      firebase.on('child_added', function(n){
+      firebase.child( 'apps/' ).on('child_added', function(n){
         cb();
       });
     },
-    onChange: function( appId, cb ){
-      firebase.child( appId ).on('child_changed', function(n){
+    onChange: function(appId, cb ){
+      firebase.child(  'apps/' + appId ).on('child_changed', function(n){
         cb();
       });
     }
   }
 }
+
+function getAllApps( cb ){
+  appKeys = {};
+  firebase.child('apps').orderByKey().on('value', function(data){
+    // store for use later
+    appKeys[data.key()] = data.val().name;
+
+    debug('app.getAll>', data);
+    cb(null, data);
+  })
+}
+
+function checkId(id){
+  if (!_.isUndefined(getAppKeyFromName(id))){
+    // do we have a name?
+    console.warn('App Name passed')
+    return getAppKeyFromName(id);
+  }
+  if (!checkAppId(appId)){
+    debug('No Application ID Found', id);
+    return false;
+  }
+  return id;
+}
+
+//check app ids
+function checkAppId(id){
+  return appKeys.hasOwnProperty(id);
+}
+
+function getAppKeyFromName(name){
+  return _.findKey( appKeys, { name: name })
+}
+
+var appKeys = {};
+
+function genID(){
+  return _.map(Array(16), function(v, i){
+    return Math.floor(Math.random()*16).toString(16)
+  }).join( '' )
+}
+console.log(genID())
