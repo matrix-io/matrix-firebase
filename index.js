0552c21ba573b970f5fc98f71f16a96833b4b316
Sean Canton <eighteyes@gmail.com>
2016-10-21 09:53:24 -0700

    Added error check in case GH goes down. Upgrade.

--- index.js
+++ index.js
@@ -18,31 +18,30 @@ var e = {};
     e[f.slice(0, -3)] = require('./lib/' + f);
   })
 
-  // do version check on debug
-  if ( process.env.hasOwnProperty('DEBUG')){
-    var msg;
-    var info = JSON.parse(require('fs').readFileSync(__dirname + '/package.json'));
-    var currentVersion = info.version;
-    require('https').get(
-      'https://raw.githubusercontent.com/matrix-io/matrix-firebase/master/package.json',
-    function (res) {
-      var write = "";
-      res.on('data', function (c) {
-        write += c;
-      });
-      res.on('end', function () {
-        var remoteVersion = JSON.parse(write).version;
-        if (currentVersion === remoteVersion) {
-          msg = '(current)'.grey;
-          e.current = true;
-        } else {
-          e.current = false;
-          msg = '(can upgrade to '.yellow+ remoteVersion +')'.yellow
-        }
-        debug( '🔥  [ MATRIX ] Firebase v'.red + currentVersion.grey, msg )
-      });
-    })
-  }
+  var msg;
+  var info = JSON.parse(require('fs').readFileSync(__dirname + '/package.json'));
+  var currentVersion = info.version;
+  require('https').get(
+    'https://raw.githubusercontent.com/matrix-io/matrix-firebase/master/package.json',
+  function (res) {
+    var write = "";
+    res.on('data', function (c) {
+      write += c;
+    });
+    res.on('end', function () {
+      var remoteVersion = JSON.parse(write).version;
+      if (currentVersion === remoteVersion) {
+        msg = '(current)'.grey;
+        e.current = true;
+      } else {
+        e.current = false;
+        msg = '(can upgrade to '.yellow+ remoteVersion +')'.yellow
+      }
+      debug( '🔥  [ MATRIX ] Firebase v'.red + currentVersion.grey, msg )
+    });
+  }).on('error', function (e) {
+    console.error('Upgrade Check Error: ', e)
+  })
 
 // shortcut for API sanity
 e.init = e.util.init;
